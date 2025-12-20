2d23ac76a6c603fcc9400201458fc45ab74a0c6b
Rodrigo Cunha <wkm.rcunha@terceiros.mag.com.br>
2025-12-20 15:29:44 -0300

    feat: cria componente Header, Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import { Button } from "@mui/material"
+import { Box } from "@mui/system";
 import "./App.css"
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({});
 
 export const App = () => {
-  return <Button variant="contained">Hello World!</Button>
+  return (
+    <Stack spacing={2} sx={{ flexGrow: 1 }}>
+      <ThemeProvider theme={ theme }>
+        <Box
+          component="main"
+          sx={{
+            height: "100vh",
+            backgroundColor: "white"
+          }}
+        >
+          <Header />
+          <Layout>
+            <h1>Olá mundo</h1>
+          </Layout>
+        </Box>
+      </ThemeProvider>
+    </Stack>
+  );
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import { Container } from "@mui/system";
+
+export default function Layout({ children } : { children: React.ReactNode }) {
+    return (
+        <Box>
+            <Container
+                maxWidth="lg"
+                sx={{
+                    mt: 4,
+                    mb: 4,
+                    backgroundColor: "white"
+                }}
+            >
+                { children }
+            </Container>
+        </Box>
+    );
+}
