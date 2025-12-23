afce43f6e58b2eeb5f57287b2ec0241babeee945
Rodrigo Cunha <wkm.rcunha@terceiros.mag.com.br>
2025-12-23 20:47:54 -0300

    config theme e react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,51 @@ import "./App.css"
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 import Stack from '@mui/material/Stack';
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import { appTheme } from "./config/theme";
+import { Typography } from "@mui/material";
 
-const theme = createTheme({});
+import {
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const Home = () => (
+  <Box>
+    <Typography>
+      Home
+    </Typography>
+  </Box>
+);
+
+const About = () => (
+  <Box>
+    <Typography>
+      About
+    </Typography>
+  </Box>
+);
 
 export const App = () => {
   return (
     <Stack spacing={2} sx={{ flexGrow: 1 }}>
-      <ThemeProvider theme={ theme }>
+      <ThemeProvider theme={ appTheme }>
         <Box
           component="main"
           sx={{
             height: "100vh",
-            backgroundColor: "white"
+            backgroundColor: (theme) => theme.palette.grey[900],
           }}
         >
           <Header />
           <Layout>
-            <h1>Olá mundo</h1>
+            <h1>Welcome to react router!</h1>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route />
+            </Routes>
           </Layout>
         </Box>
       </ThemeProvider>
